b6b21b6ed018818476bffbe0c4f16d659d6a43f4
Do Gia Dat <d.dat@id-entity.vn>
2019-02-20 18:10:14 +0700

    Updates

---
+++ precache-manifest.779119c0663b55e38967c08f36124e6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c3af9d7e88abdfb8c56c",
+    "url": "/test-app/static/js/runtime~main.c3af9d7e.js"
+  },
+  {
+    "revision": "04c3518aeeddc2092b56",
+    "url": "/test-app/static/js/main.04c3518a.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/test-app/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "04c3518aeeddc2092b56",
+    "url": "/test-app/static/css/main.f77dc058.chunk.css"
+  },
+  {
+    "revision": "e762da0ee98a4a109e43604a0b3a4ea6",
+    "url": "/test-app/index.html"
+  }
+];
